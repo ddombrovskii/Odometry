494897e7e3a4404b6adfb8f9b653eb7e23f4a991
khoroshev_daniil <daniil.khoroshev@netcracker.com>
2023-02-06 23:09:22 +0400

    added tests on non binary images

--- find_path_c.py
+++ find_path_c.py
@@ -1,6 +1,6 @@
 from ctypes import CDLL, Structure, c_int, c_float, POINTER, Array
 from typing import Any
-
+from PIL import Image, ImageDraw
 import cv2
 import numpy as np
 
@@ -34,73 +34,41 @@ def print_path_data(path_p: Path):
         print(f"({path_p.contents.path_points[i].row}, {path_p.contents.path_points[i].col})")
 
 
-def test_path_finder():
+def test_path_finder_on_binary_map():
     _F = 1.0
     _X = 1000.0
-    py_weights = [_F, _X, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F,
-                  _X, _F, _F, _F, _F, _F, _F,
-                  _F, _X, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F,
-                  _X, _F, _F, _F, _F, _F, _F,
-                  _F, _X, _F, _X, _F, _F, _F, _F, _F, _X, _F, _F, _F, _X, _F, _F, _F, _X, _F, _X, _F, _F, _F, _F, _F,
-                  _X, _F, _F, _F, _X, _F, _F,
-                  _F, _X, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _X, _F, _X, _F, _F, _F, _F, _F,
-                  _F, _F, _F, _F, _X, _F, _F,
-                  _F, _X, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _X, _F, _X, _F, _F, _F, _F, _F,
-                  _F, _F, _F, _F, _X, _F, _F,
-                  _F, _F, _F, _X, _F, _X, _X, _X, _X, _X, _X, _F, _F, _X, _F, _F, _F, _F, _F, _X, _F, _X, _X, _X, _X,
-                  _X, _X, _F, _F, _X, _F, _F,
-                  _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _X, _F, _F, _X, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F,
-                  _F, _X, _F, _F, _X, _F, _F,
-                  _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F,
-                  _F, _X, _F, _F, _X, _F, _F,
-                  _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F,
-                  _F, _X, _F, _F, _X, _F, _F,
-                  _F, _X, _F, _X, _X, _X, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _X, _F, _X, _X, _X, _F, _F, _F,
-                  _F, _X, _F, _F, _F, _F, _F,
-                  _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F,
-                  _F, _X, _F, _F, _F, _F, _F,
-                  _F, _X, _F, _F, _F, _F, _F, _X, _X, _F, _X, _F, _X, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _X, _X,
-                  _F, _X, _F, _X, _F, _F, _F,
-                  _F, _X, _X, _X, _X, _F, _F, _X, _X, _F, _X, _F, _X, _F, _F, _F, _F, _X, _X, _X, _X, _F, _F, _X, _X,
-                  _F, _X, _F, _X, _F, _F, _F,
-                  _F, _F, _F, _F, _F, _F, _F, _X, _X, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _X,
-                  _F, _F, _F, _X, _F, _F, _F,
-                  _F, _F, _F, _F, _F, _F, _F, _X, _X, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _X,
-                  _F, _F, _F, _X, _F, _F, _F,
-                  _F, _F, _F, _F, _F, _F, _F, _X, _X, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _X,
-                  _F, _F, _F, _X, _F, _F, _F,
-                  _F, _X, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F,
-                  _X, _F, _F, _F, _F, _F, _F,
-                  _F, _X, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F,
-                  _X, _F, _F, _F, _F, _F, _F,
-                  _F, _X, _F, _X, _F, _F, _F, _F, _F, _X, _F, _F, _F, _X, _F, _F, _F, _X, _F, _X, _F, _F, _F, _F, _F,
-                  _X, _F, _F, _F, _X, _F, _F,
-                  _F, _X, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _X, _F, _X, _F, _F, _F, _F, _F,
-                  _F, _F, _F, _F, _X, _F, _F,
-                  _F, _X, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _X, _F, _X, _F, _F, _F, _F, _F,
-                  _F, _F, _F, _F, _X, _F, _F,
-                  _F, _F, _F, _X, _F, _X, _X, _X, _X, _X, _X, _F, _F, _X, _F, _F, _F, _F, _F, _X, _F, _X, _X, _X, _X,
-                  _X, _X, _F, _F, _X, _F, _F,
-                  _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _X, _F, _F, _X, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F,
-                  _F, _X, _F, _F, _X, _F, _F,
-                  _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F,
-                  _F, _X, _F, _F, _X, _F, _F,
-                  _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F,
-                  _F, _X, _F, _F, _X, _F, _F,
-                  _F, _X, _F, _X, _X, _X, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _X, _F, _X, _X, _X, _F, _F, _F,
-                  _F, _X, _F, _F, _F, _F, _F,
-                  _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F,
-                  _F, _X, _F, _F, _F, _F, _F,
-                  _F, _X, _F, _F, _F, _F, _F, _X, _X, _F, _X, _F, _X, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _X, _X,
-                  _F, _X, _F, _X, _F, _F, _F,
-                  _F, _X, _X, _X, _X, _F, _F, _X, _X, _F, _X, _F, _X, _F, _F, _F, _F, _X, _X, _X, _X, _F, _F, _X, _X,
-                  _F, _X, _F, _X, _F, _F, _F,
-                  _F, _F, _F, _F, _F, _F, _F, _X, _X, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _X,
-                  _F, _F, _F, _X, _F, _F, _F,
-                  _F, _F, _F, _F, _F, _F, _F, _X, _X, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _X,
-                  _F, _F, _F, _X, _F, _F, _F,
-                  _F, _F, _F, _F, _F, _F, _F, _X, _X, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _X,
-                  _F, _F, _F, _X, _F, _F, _F
+    py_weights = [_F, _X, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F,
+                  _F, _X, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F,
+                  _F, _X, _F, _X, _F, _F, _F, _F, _F, _X, _F, _F, _F, _X, _F, _F, _F, _X, _F, _X, _F, _F, _F, _F, _F, _X, _F, _F, _F, _X, _F, _F,
+                  _F, _X, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _X, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F,
+                  _F, _X, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _X, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F,
+                  _F, _F, _F, _X, _F, _X, _X, _X, _X, _X, _X, _F, _F, _X, _F, _F, _F, _F, _F, _X, _F, _X, _X, _X, _X, _X, _X, _F, _F, _X, _F, _F,
+                  _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _X, _F, _F, _X, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _X, _F, _F, _X, _F, _F,
+                  _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _X, _F, _F,
+                  _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _X, _F, _F,
+                  _F, _X, _F, _X, _X, _X, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _X, _F, _X, _X, _X, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F,
+                  _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F,
+                  _F, _X, _F, _F, _F, _F, _F, _X, _X, _F, _X, _F, _X, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _X, _X, _F, _X, _F, _X, _F, _F, _F,
+                  _F, _X, _X, _X, _X, _F, _F, _X, _X, _F, _X, _F, _X, _F, _F, _F, _F, _X, _X, _X, _X, _F, _F, _X, _X, _F, _X, _F, _X, _F, _F, _F,
+                  _F, _F, _F, _F, _F, _F, _F, _X, _X, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _X, _F, _F, _F, _X, _F, _F, _F,
+                  _F, _F, _F, _F, _F, _F, _F, _X, _X, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _X, _F, _F, _F, _X, _F, _F, _F,
+                  _F, _F, _F, _F, _F, _F, _F, _X, _X, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _X, _F, _F, _F, _X, _F, _F, _F,
+                  _F, _X, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F,
+                  _F, _X, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F,
+                  _F, _X, _F, _X, _F, _F, _F, _F, _F, _X, _F, _F, _F, _X, _F, _F, _F, _X, _F, _X, _F, _F, _F, _F, _F, _X, _F, _F, _F, _X, _F, _F,
+                  _F, _X, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _X, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F,
+                  _F, _X, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _X, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F,
+                  _F, _F, _F, _X, _F, _X, _X, _X, _X, _X, _X, _F, _F, _X, _F, _F, _F, _F, _F, _X, _F, _X, _X, _X, _X, _X, _X, _F, _F, _X, _F, _F,
+                  _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _X, _F, _F, _X, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _X, _F, _F, _X, _F, _F,
+                  _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _X, _F, _F,
+                  _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _X, _F, _F,
+                  _F, _X, _F, _X, _X, _X, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _X, _F, _X, _X, _X, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F,
+                  _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F,
+                  _F, _X, _F, _F, _F, _F, _F, _X, _X, _F, _X, _F, _X, _F, _F, _F, _F, _X, _F, _F, _F, _F, _F, _X, _X, _F, _X, _F, _X, _F, _F, _F,
+                  _F, _X, _X, _X, _X, _F, _F, _X, _X, _F, _X, _F, _X, _F, _F, _F, _F, _X, _X, _X, _X, _F, _F, _X, _X, _F, _X, _F, _X, _F, _F, _F,
+                  _F, _F, _F, _F, _F, _F, _F, _X, _X, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _X, _F, _F, _F, _X, _F, _F, _F,
+                  _F, _F, _F, _F, _F, _F, _F, _X, _X, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _X, _F, _F, _F, _X, _F, _F, _F,
+                  _F, _F, _F, _F, _F, _F, _F, _X, _X, _F, _F, _F, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X, _X, _F, _F, _F, _X, _F, _F, _F
                   ]
 
     c_weights = (c_float * len(py_weights))(*py_weights)
@@ -118,6 +86,51 @@ def test_path_finder():
 
     path_del_func(path_p)
     print("Path pointer was deleted")
+
+
+def test_path_finder_on_non_binary_map():
+    _F = 1.0
+    _X = 1000.0
+    _B = 500.0
+
+    weights_7x30 = [
+        _F, _F, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X,
+        _F, _F,
+        _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _B, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F,
+        _F, _F,
+        _X, _F, _X, _X, _F, _F, _F, _F, _F, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F,
+        _F, _X,
+        _X, _B, _X, _X, _F, _F, _F, _F, _F, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F,
+        _F, _X,
+        _X, _F, _F, _F, _F, _X, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F,
+        _F, _X,
+        _X, _F, _F, _F, _F, _X, _X, _F, _F, _F, _F, _F, _F, _X, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F,
+        _F, _F,
+        _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X,
+        _F, _F
+    ]
+    cols = 30
+    rows = 7
+    c_weights = (c_float * len(weights_7x30))(*weights_7x30)
+
+    tests_3 = [
+        (Pt(0, 0), Pt(4, 1)),
+        (Pt(0, 0), Pt(1, 14)),
+        (Pt(0, 0), Pt(0, 29)),
+        (Pt(0, 0), Pt(6, 29))
+    ]
+
+    mew_map = Map(c_int(cols), c_int(rows), c_weights)
+    for points in tests_3:
+        start_point = points[0]
+        end_point = points[1]
+
+        print(f"start point: ({start_point.row}, {start_point.col})")
+        print(f"end point: ({end_point.row}, {end_point.col})")
+
+        path_p = find_path_func(mew_map, start_point, end_point)
+        print_path_data(path_p)
+        path_del_func(path_p)
 
 
 def get_weights_from_binary_image(img_path: str, scale: float = 0.25) -> [Array[Any], int, int, list]:
@@ -139,6 +152,23 @@ def get_weights_from_binary_image(img_path: str, scale: float = 0.25) -> [Array[
     py_weights += 1
     c_weights = (c_float * len(py_weights))(*py_weights)
     return c_weights, resized_image.shape[0], resized_image.shape[1], resized_image
+
+
+def get_weights_from_non_binary_image(img_path: str, scale: float = 0.25) -> [Array[Any], int, int, Image.Image]:
+    img = Image.open(img_path)
+    print(f"{scale=}")
+    print(f"original image shape: {img.size}")
+    resized_image = img.resize((int(img.size[0]*scale), int(img.size[1]*scale)))
+    print(f"resized image shape: {resized_image.size}")
+    img_arr = np.asarray(resized_image)
+    img_arr = cv2.bitwise_not(img_arr)
+    py_weights = []
+    for row in img_arr:
+        py_weights += list(row)
+    py_weights = np.array(py_weights, dtype=int)
+    py_weights += 1
+    c_weights = (c_float * len(py_weights))(*py_weights)
+    return c_weights, img_arr.shape[0], img_arr.shape[1], resized_image
 
 
 def print_weights(c_weights: Array[Any], rows: int, cols: int):
@@ -168,47 +198,42 @@ def select_point(event, x, y, flags, param):
 
 
 def show_path(image, path_p: POINTER(Path), start_point, end_point):
-    image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
-    for i in range(path_p.contents.n_points):
-        x = path_p.contents.path_points[i].col
-        y = path_p.contents.path_points[i].row
+    if not isinstance(image, Image.Image):
+        image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+        for i in range(path_p.contents.n_points):
+            x = path_p.contents.path_points[i].col
+            y = path_p.contents.path_points[i].row
 
-        # отмечаю выбранные точки на картинке
-        image = cv2.circle(image, (x, y), radius=1, color=(255, 0, 0), thickness=-1)
+            # отмечаю выбранные точки на картинке
+            image = cv2.circle(image, (x, y), radius=1, color=(255, 0, 0), thickness=-1)
 
-    image = cv2.circle(image, (start_point.col, start_point.row), radius=3, color=(0, 255, 0), thickness=-1)
-    image = cv2.circle(image, (end_point.col, end_point.row), radius=3, color=(0, 0, 255), thickness=-1)
-    cv2.imshow("path", image)
-    cv2.waitKey(0)
+        image = cv2.circle(image, (start_point.col, start_point.row), radius=3, color=(0, 255, 0), thickness=-1)
+        image = cv2.circle(image, (end_point.col, end_point.row), radius=3, color=(0, 0, 255), thickness=-1)
+        cv2.imshow("path", image)
+        cv2.waitKey(0)
+    else:
+        rgb_image = image.convert('RGB')
+        draw = ImageDraw.Draw(rgb_image)
+        for i in range(path_p.contents.n_points):
+            x = path_p.contents.path_points[i].col
+            y = path_p.contents.path_points[i].row
+
+            draw.point((x, y), fill='blue')
+        draw.point((start_point.col, start_point.row), fill='green')
+        draw.point((start_point.col, start_point.row), fill='red')
+        rgb_image.show("Founded path")
 
 
 def test_path_finder_on_map_from_img():
     # c_weights, rows, cols, resized_image = get_weights_from_binary_image("./mapping/map1.png")
     # c_weights, rows, cols, resized_image = get_weights_from_binary_image("./mapping/map_low_res.png", scale=1.0)
-    # c_weights, rows, cols, resized_image = get_weights_from_binary_image("./mapping/map_blured.png")
+    c_weights, rows, cols, resized_image = get_weights_from_non_binary_image("./mapping/map_blured.png")
 
     # создаю окно для получения координат начальной и конечной точек (дабл клик ЛКМ)
     # cv2.namedWindow("select point")
     # cv2.setMouseCallback("select point", select_point)
     # cv2.imshow("select point", resized_image)
     # cv2.waitKey(0)
-
-    _F = 1.0
-    _X = 1000.0
-    _B = 500.0
-
-    weights_7x30 = [
-        _F, _F, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _F, _F,
-        _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _B, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F,
-        _X, _F, _X, _X, _F, _F, _F, _F, _F, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X,
-        _X, _B, _X, _X, _F, _F, _F, _F, _F, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X,
-        _X, _F, _F, _F, _F, _X, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _X,
-        _X, _F, _F, _F, _F, _X, _X, _F, _F, _F, _F, _F, _F, _X, _X, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F, _F,
-        _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _X, _F, _F
-    ]
-    cols = 30
-    rows = 7
-    c_weights = (c_float * len(weights_7x30))(*weights_7x30)
 
     tests_1 = [
         (Pt(5, 97), Pt(139, 32)),
@@ -236,17 +261,9 @@ def test_path_finder_on_map_from_img():
         (Pt(3, 2), Pt(94, 66)),
     ]
 
-    tests_3 = [
-        (Pt(0, 0), Pt(4, 1)),
-        (Pt(0, 0), Pt(1, 14)),
-        (Pt(0, 0), Pt(0, 29)),
-        (Pt(0, 0), Pt(6, 29))
-    ]
-
     map_from_img = Map(c_int(cols), c_int(rows), c_weights)
-    # a_star_lib.print_map(map_from_img)
 
-    for points in tests_3:
+    for points in tests_1:
         start_point = points[0]
         end_point = points[1]
 
@@ -254,12 +271,13 @@ def test_path_finder_on_map_from_img():
         print(f"end point: ({end_point.row}, {end_point.col})")
 
         path_p = find_path_func(map_from_img, start_point, end_point)
-        print_path_data(path_p)
-        # show_path(resized_image, path_p, start_point, end_point)
+        # print_path_data(path_p)
+        show_path(resized_image, path_p, start_point, end_point)
         path_del_func(path_p)
 
 
 if __name__ == "__main__":
-    # test_path_finder()
+    # test_path_finder_on_binary_map()
+    # test_path_finder_on_non_binary_map()
 
     test_path_finder_on_map_from_img()
